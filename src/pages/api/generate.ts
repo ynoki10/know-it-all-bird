527203d73a5c1c7a5cd93f5bf0e9aef06f09a53e
ynoki10 <sakanafront@gmail.com>
2023-04-21 08:59:43 +0900

    不要console削除

--- src/pages/api/generate.ts
+++ src/pages/api/generate.ts
@@ -42,14 +42,11 @@ const generate = async (req: NextApiRequest, res: NextApiResponse) => {
         ],
         temperature: 0.8,
       });
-      console.log(response.data.choices[0].message?.content);
       res.status(200).json({ result: response.data.choices[0].message?.content });
     } catch (error: any) {
       if (error.response) {
-        console.error(error.response.status, error.response.data);
         res.status(error.response.status).json(error.response.data);
       } else {
-        console.error(`Error with OpenAI API request: ${error.message}`);
         res.status(500).json({
           error: {
             message: 'An error occurred during your request.',
